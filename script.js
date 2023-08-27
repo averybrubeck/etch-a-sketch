63e81558f682c2f39d425eaf17102bdbfb602b2f
averybrubeck <averybrubeck@yahoo.com>
2023-08-27 11:26:24 -0400

    grid generation complete

--- script.js
+++ script.js
@@ -1,13 +1,21 @@
-const gridItem = document.querySelector('.grid-item');
-const newItem = document.createElement(gridItem)
-const grid = document.querySelector('.grid');
-const container = document.querySelector('.container');
+const grid = document.querySelector('#grid');
+const container = document.querySelector('#container');
 
-function makeGrid(rows){
-    for(let i = 0; i <= rows; i++){
-        container.appendChild(newItem);
-    }
-
+function createGridItem(content){
+    const newItem = document.createElement('div');
+    newItem.classList.add('grid-item');
+    return newItem
 }
 
-makeGrid(8);
+function makeGrid(content){
+    const newItem = createGridItem(content);
+    grid.appendChild(newItem);
+}
+
+function moreGrid(rows){
+    for(let i = 0; i <= rows; i++){
+        
+        makeGrid();
+    }
+}
+moreGrid(24);
